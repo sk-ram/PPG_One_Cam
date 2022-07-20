d132e611ab8474c2ea629537240a11dd4e756e27
Srikark-17 <srikar.sgk@gmail.com>
2022-07-19 22:11:03 -0500

    Added logic to email verification

--- app/src/main/java/com/example/PWV/email_verification_activity.kt
+++ app/src/main/java/com/example/PWV/email_verification_activity.kt
@@ -4,16 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class email_verification_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.email_verification)
+        val user = Firebase.auth.currentUser
 
         var contineToMainActivity= findViewById<Button>(R.id.button3)
         contineToMainActivity.setOnClickListener {
             val intent = Intent(this, dashboard_activity::class.java)
-            startActivity(intent)
+            if (user?.isEmailVerified == true) {
+                startActivity(intent)
+            }
         }
     }
 }
